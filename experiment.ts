d66169c9a018f45a0c022e197c534951c7145bf2
Lee MinGi <mingi3442@gmail.com>
2024-12-14 19:31:36 +0900

    🚧 Get a balance

--- experiment.ts
+++ experiment.ts
@@ -1,10 +1,12 @@
 import { StargateClient } from "@cosmjs/stargate"
+import { config } from "dotenv"
 
-const COSMOS_HUB_RPC_URL = "https://cosmos-rpc.polkachu.com/"
+config()
 
 const runAll = async (): Promise<void> => {
-    const client = await StargateClient.connect(COSMOS_HUB_RPC_URL)
+    const client = await StargateClient.connect(process.env.COSMOS_HUB_TESTNET_RPC ?? "")
     console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+    console.log("Alice balances:", await client.getAllBalances(process.env.ALICE_ADDRESS ?? ""))
 }
 
 runAll()
