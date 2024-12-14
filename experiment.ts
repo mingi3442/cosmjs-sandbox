ff6f3757ce00136e5789a68bdcd82efd8782d4c5
Lee MinGi <mingi3442@gmail.com>
2024-12-14 18:42:56 +0900

    🚧 Define connection

--- experiment.ts
+++ experiment.ts
@@ -1,5 +1,10 @@
+import { StargateClient } from "@cosmjs/stargate"
+
+const COSMOS_HUB_RPC_URL = "https://cosmos-rpc.polkachu.com/"
+
 const runAll = async (): Promise<void> => {
-    console.log("TODO")
+    const client = await StargateClient.connect(COSMOS_HUB_RPC_URL)
+    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
 }
 
 runAll()
